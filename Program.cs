ebc89ea3f051c899d60b09a7cf7516adaa6c9e65
mohammadaminnasiri131383@gmail.com <mohammadaminnasiri131383@gmail.com>
2024-07-04 19:47:38 +0330

    added structure class | Amin Nasiri

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Final_Project_Graph
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Node node = new Node();
+        }
+        
+    }
+}
---
+++ Stracture.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Final_Project_Graph
+{
+	
+        public class Node
+        {
+            public string Identifier { get; set; }
+            public bool IsGateway { get; set; }
+            public List<Node> Neighbors { get; set; }
+
+            public Node(string identifier, bool isGateway = false)
+            {
+                Identifier = identifier;
+                IsGateway = isGateway;
+                Neighbors = new List<Node>();
+            }
+        }
+
+        public class Area
+        {
+            public string Identifier { get; set; }
+            public bool IsSafe { get; set; }
+            public List<Node> Nodes { get; set; }
+            public Node Gateway { get; set; }
+
+            public Area(string identifier, bool isSafe)
+            {
+                Identifier = identifier;
+                IsSafe = isSafe;
+                Nodes = new List<Node>();
+            }
+        }
+
+        public class WAN
+        {
+            public List<Area> Areas { get; set; }
+
+            public WAN()
+            {
+                Areas = new List<Area>();
+            }
+
+            public void AddArea(Area area)
+            {
+                Areas.Add(area);
+            }
+
+            public void RouteMessage(string sourceAreaId, string destinationAreaId, string message)
+            {
+                // Implement routing logic here
+            }
+        }
+}
+
